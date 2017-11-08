c1e95bcebdbda5d0d28813d5e50bdd9cadaa5fd7
Dieter Schalk <gargamelk@hotmail.com>
2017-11-08 17:00:32 +0100

    Finished

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+gem 'faker'
 # gem 'jquery-rails' # Add this line if you use Rails 5.1
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Coffee.blend_name,
+    address: ["Amsterdam", "Paris", "London", "Shanghai", "Tokio", "Belgrade"].sample,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+  restaurant.save!
+  5.times do
+    review = Review.new(
+      restaurant: restaurant,
+      content: Faker::TwinPeaks.quote,
+      rating: (1..5).to_a.sample
+      )
+    review.save!
+  end
+end
+
+puts 'Finished!'
+
+  # {
+  #   name:         "Epicure au Bristol",
+  #   address:      "112 rue du Fg St-Honoré 75008 Paris",
+  #   description:  "Face au jardin, on découvre une salle lumineuse... et la cuisine d'Éric Frechon.",
+  #   stars:        3
+  # },
+  # {
+  #   name:         "La truffière",
+  #   address:      "4 rue Blainville 75005 Paris",
+  #   description:  "Une valeur sûre que cette belle maison du 17e et les recettes de Jean-Christophe Rizet",
+  #   stars:        1
+  # },
+  # {
+  #   name:         "Le pré catelan",
+  #   address:      "route de Suresnes 75016 Paris",
+  #   description:  "Oeil vif, geste sûr: impossible de distinguer, dans les créations de Frédéric Anton...",
+  #   stars:        3
+  # }
